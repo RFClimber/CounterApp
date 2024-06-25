ca60f797d10f342f2c0cc5623b4428b720f612e4
RFClimber <choiwoolim98@gmail.com>
2024-06-25 14:24:11 +0900

    codeBase UI

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private var num: Int = 0
+    
+    let counter = UILabel()
+    let minusButton = UIButton()
+    let plusButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+       
+    }
 
-        // Do any additional setup after loading the view.
+    private func configureUI() {
+        view.backgroundColor = .black
+        
+        counter.text = "\(self.num)"
+        counter.textColor = .white
+        counter.font = UIFont.boldSystemFont(ofSize: 45)
+        counter.textAlignment = .center
+        
+        minusButton.backgroundColor = .red
+        minusButton.setTitle("감소", for: .normal)
+        minusButton.setTitleColor(UIColor.white, for: .normal)
+        minusButton.layer.cornerRadius = 8
+        minusButton.addTarget(self, action: #selector(minusbuttonClicked), for: .touchDown)
+        
+        plusButton.backgroundColor = .blue
+        plusButton.setTitle("증가", for: .normal)
+        plusButton.setTitleColor(UIColor.white, for: .normal)
+        plusButton.layer.cornerRadius = 8
+        plusButton.addTarget(self, action: #selector(plusbuttonClicked), for: .touchDown)
+        
+        [counter, minusButton, plusButton]
+            .forEach { view.addSubview($0) }
+        
+        counter.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.center.equalToSuperview()
+        }
+        minusButton.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.height.equalTo(30)
+            $0.centerY.equalTo(counter)
+            $0.right.equalTo(counter.snp.left).inset(-32)
+        }
+        plusButton.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.height.equalTo(30)
+            $0.centerY.equalTo(counter)
+            $0.left.equalTo(counter.snp.right).offset(32)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc
+    private func minusbuttonClicked() {
+        self.num -= 1
+        counter.text = "\(self.num)"
     }
-    */
-
+    @objc
+    private func plusbuttonClicked() {
+        self.num += 1
+        counter.text = "\(self.num)"
+    }
 }
